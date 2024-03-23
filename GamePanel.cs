1e245f0e0026c936337d9061eb64a9f59787581a
Cezar Bulancea <118464357+Cezarrr9@users.noreply.github.com>
2024-03-23 17:46:56 +0100

    comments game panel

--- GamePanel.cs
+++ GamePanel.cs
@@ -1,27 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Snake
 {
     public class GamePanel : Panel
     {
+        // List of all the points in the snake
         public List<Point> Snake { get; set; }
+
+        // List of all the moves that await to be executed 
         public List<Direction> Moves { get; set; }
+
+        // The position of the food
         public Point Food { get; set; }
+
+        // Timer which tracks the speed at which the 
+        // game panel is updated 
         public Timer GameTimer { get; set; }
+
+        // Timer which keeps track of the seconds that 
+        // have passed since the beginning of the game 
         public Timer CountTimer { get; set; }
+
+        // The score of the player (or how much food the snake ate)
         public int Score { get; set; }
+
+        // The number of seconds that have passed
+        // since the beginning of the game
         public int Time { get; set; }
+
+        // The size of a cell inside the game panel
         public const int CELL_SIZE = 20;
+
+        // The current direction in which the snake is moving
         public Direction CurrentDirection { get; set; }
 
+        // Event for changing the  score label
         public event Action<int> ScoreChanged;
+
+        // Event for changing the time label
         public event Action<int> TimeChanged;
+
+        // Event for when the game has stopped
         public event Action GameStopped;
 
         public GamePanel()
@@ -32,6 +54,9 @@ namespace Snake
             GameTimer = new Timer();
         }
 
+        /// <summary>
+        /// Gets the score, time and current direction to their initial values.
+        /// </summary>
         private void ResetGame()
         {
             Score = 0;
@@ -41,9 +66,14 @@ namespace Snake
             CurrentDirection = Direction.Left;
         }
 
+        /// <summary>
+        /// Generates a random location for the food to be placed. 
+        /// </summary>
         private void PlaceFood()
         {
             var rnd = new Random();
+            // It continues to generate random points until
+            // it finds one that is not on the snake.
             do
             {
                 int x = rnd.Next(Width / CELL_SIZE);
@@ -52,18 +82,33 @@ namespace Snake
             } while (Snake.Contains(Food));
         }
 
+        /// <summary>
+        /// Place the snake in the center of the panel.
+        /// </summary>
         private void PlaceSnake()
         {
             Snake.Clear();
             Snake.Add(new Point(10, 10));
         }
 
+        /// <summary>
+        /// Increases the number of seconds that have passed since
+        /// the beginning of the game and invokes the event that changes 
+        /// the time label with the new value.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void UpdateTime(object sender, EventArgs e)
         {
             Time += 1;
             TimeChanged?.Invoke(Time);
         }
 
+        /// <summary>
+        /// Updates the state of the game.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void UpdateGame(object sender, EventArgs e)
         {
             // Update snake's position based on currentDirection
@@ -129,16 +174,23 @@ namespace Snake
                     GameStopped?.Invoke();
                     return;
                 }
+
+                // If they didn't win, then generate new food
+                // and increase the speed of the snake.
                 PlaceFood();
                 GameTimer.Interval -= 1;
             }
 
+            // Empty the list of moves 
             Moves.Clear();
 
             // If the game is still on, redraw
             Invalidate();
         }
 
+        /// <summary>
+        /// Declares the game timer.
+        /// </summary>
         private void InitializeGameTimer()
         {
             GameTimer = new Timer();
@@ -147,6 +199,9 @@ namespace Snake
             GameTimer.Start();
         }
 
+        /// <summary>
+        /// Declares the count timer.
+        /// </summary>
         private void InitializeCountTimer()
         {
             CountTimer = new Timer();
@@ -155,6 +210,9 @@ namespace Snake
             CountTimer.Start();
         }
 
+        /// <summary>
+        /// Starts a game. 
+        /// </summary>
         public void InitializeGame()
         {
             ResetGame();
